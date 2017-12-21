6d46fc093278495f29284e09b5150b5a56ca7ab7
sergii <sergii@antvoice.com>
2017-12-21 09:26:31 +0100

    day 2

---
+++ AdventOfCode2017/Day2.fs
@@ -0,0 +1,16 @@
+﻿module Day2
+
+open System
+open System.IO
+
+let input = File.ReadAllLines(sprintf "%s\data\day2.txt" __SOURCE_DIRECTORY__)
+
+let getDiffMinMax (line : int array) = 
+    let (min, max) = line |> Array.fold (fun (min, max) item -> (Math.Min(min, item), Math.Max(max, item))) (line.[0], line.[0])
+    max - min
+
+let result = 
+    input 
+    |> Array.map(fun line -> line.Split('\t') |> Array.map Int32.Parse)
+    |> Array.map getDiffMinMax 
+    |> Array.sum
