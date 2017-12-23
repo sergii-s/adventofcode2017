88f4cf37b78afa6b0f034b6005317d249fe4f091
ssergii <sergii.salata@gmail.com>
2017-12-23 21:54:36 +0100

    day4

---
+++ AdventOfCode2017/day4.fs
@@ -0,0 +1,25 @@
+﻿module Day4
+
+open System
+open System.IO
+
+let input = File.ReadAllLines(sprintf "%s\data\day4.txt" __SOURCE_DIRECTORY__)
+
+let part1 = 
+    let isValidPassPhrase words = 
+        let distinctWords = words |> Array.distinct
+        words.Length = distinctWords.Length
+    input 
+    |> Seq.map(fun line -> line.Split([|" "|],StringSplitOptions.RemoveEmptyEntries))
+    |> Seq.filter isValidPassPhrase
+    |> Seq.length
+        
+let part2 = 
+    let isValidPassPhrase words = 
+        let distinctWords = words |> Array.map(fun (word:string) -> word.ToCharArray() |> Array.sort |> String) |> Array.distinct
+        words.Length = distinctWords.Length
+    input 
+    |> Seq.map(fun line -> line.Split([|" "|],StringSplitOptions.RemoveEmptyEntries))
+    |> Seq.filter isValidPassPhrase
+    |> Seq.length
+
