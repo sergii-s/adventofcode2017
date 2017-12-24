f5b43c40afaca5f8c5d6c54cbdc1de5525b3d049
ssergii <sergii.salata@gmail.com>
2017-12-24 18:38:55 +0100

    day5

---
+++ AdventOfCode2017/Day5.fs
@@ -0,0 +1,33 @@
+﻿module Day5
+
+open System
+open System.IO
+
+let input = File.ReadAllLines(sprintf "%s\data\day5.txt" __SOURCE_DIRECTORY__)
+
+let modifyP1 offset = offset + 1
+
+let modifyP2 offset =
+    if offset >= 3 then
+        offset - 1
+    else
+        offset + 1
+
+let getStepsCount modify (data:int array) =
+    let rec getResult idx cnt =
+        let offset = data.[idx] 
+        data.[idx] <- modify offset
+        match idx + offset with
+        | n when n >= data.Length || n < 0 -> cnt + 1
+        | n -> getResult n (cnt + 1)
+    getResult 0 0
+    
+let part1 = 
+    input 
+    |> Array.map Int32.Parse 
+    |> getStepsCount modifyP1
+
+let part2 = 
+    input 
+    |> Array.map Int32.Parse 
+    |> getStepsCount modifyP2
