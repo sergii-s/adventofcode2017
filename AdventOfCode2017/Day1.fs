174b0bc98a7fecb430ef960b9c8deeb7f3062153
sergii <sergii@antvoice.com>
2017-12-26 09:31:52 +0100

    day1

--- AdventOfCode2017/Day1.fs
+++ AdventOfCode2017/Day1.fs
@@ -5,12 +5,15 @@ open System.IO
 
 let input = File.ReadAllText(sprintf "%s\data\day1.txt" __SOURCE_DIRECTORY__)
 
-let digits = 
-    input.ToCharArray() 
-    |> Array.map (Char.GetNumericValue >> int) 
-    |> Array.rev
-
 let result = 
-    let (_, sum) = digits |> Array.fold (fun (prev, sum) item -> (item, if prev=item then sum + item else sum)) (digits |> Array.last, 0)
-    sum
+    let getSum step (arr:int array) =
+        arr |> Array.mapi(fun i x -> (x, arr.[(i+step)%arr.Length])) |> Array.sumBy (fun (one, two) -> if one = two then one else 0)
+        
+    let digits = 
+        input.ToCharArray() 
+        |> Array.map (Char.GetNumericValue >> int) 
+    
+    let step1 = digits |> getSum 1
+    let step2 = digits |> getSum (digits.Length/2)
+    (step1, step2)
     
